9aa650f28cc0f3cc923da139b711713ec7ab225e
prathamzx <pratham.1999@yahoo.in>
2021-05-26 02:24:48 +0530

    Education content added

--- portfolio_2/src/Components/Education.js
+++ portfolio_2/src/Components/Education.js
@@ -7,11 +7,39 @@ class Education extends React.Component {
     render(){
         return(
             <div className="education-container">
+                <div className="education-label-back"></div>
                 <div className="education-label">
                     <div className="education-label-text">Education</div>
                 </div>
             <div className="education-text">
-                <div></div>
+                <div className="education-text-entry">
+                        <img className="education-text-entry-cover"
+                        src={process.env.PUBLIC_URL+"/nit-cover.jpg"}></img>
+                        <div className="education-text-entry-title">
+                            National Institute of Technology, Kurukshetra
+                        </div>
+                        <img className="education-text-entry-dp"
+                        src={process.env.PUBLIC_URL+"/nit-logo.png"}></img>
+                        <div className="education-text-entry-desc">
+                                Description comes here!
+                        </div>
+                        
+
+                </div>
+                <div className="education-text-entry">
+                        <img className="education-text-entry-cover"
+                        src={process.env.PUBLIC_URL+"/dis-cover.jpg"}></img>
+                        <div className="education-text-entry-title">
+                            Doon International School, Dehradun
+                        </div>
+                        <img className="education-text-entry-dp"
+                        src={process.env.PUBLIC_URL+"/dis-logo.jpg"}></img>
+                        <div className="education-text-entry-desc">
+                                Description comes here!
+                        </div>
+                        
+
+                </div>
             </div>
             
             </div>
---
+++ portfolio_2/src/Components/Project.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Project.css';
+
+class Project extends React.Component {
+    
+
+    render(){
+        return(
+            <div className="project-container">
+               
+            </div>
+        );
+    }
+}
+
+export default Project;
