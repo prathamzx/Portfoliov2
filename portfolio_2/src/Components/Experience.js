158772f8503544aae6718f85c61525a48de8e248
prathamzx <pratham.1999@yahoo.in>
2021-05-27 17:54:01 +0530

    Education section updated and color scheme minor changes

--- portfolio_2/src/Components/Experience.js
+++ portfolio_2/src/Components/Experience.js
@@ -9,39 +9,39 @@ class Experience extends React.Component {
             <div key={this.props.clicked}>
                 <div id="experience"></div>
             <div className="experience-container">
-               <div className="experience-label-container">
-               <div className="experience-label"></div>
-                <div className="experience-label-text">
+               
+                <div className="experience-text">
+                <div className="experience-label-container">
+               {/* <div className="experience-label"></div> */}
+                <div className="experience-label-text-back">
+                    <div className="experience-label-text">
                     <div className="experience-label-text-style">
                       Experience
                     </div>
                 </div>
                 </div>
-                <div className="experience-text">
-                   
                 </div>
                 <div className="experience-entry">
-                <div className="experience-entry-logo">
-                    <img className="experience-entry-img"
-                    src={process.env.PUBLIC_URL+"/natwest.jpg"}></img>
+                    <div className="experience-entry-logo">
+                        <img src={process.env.PUBLIC_URL + 'natwest.jpg'} 
+                        className="experience-entry-logo-img" ></img>
+                    </div>
                 </div>
-                <div className="experience-entry-connect"></div>
-                </div>
+                <div className="experience-entry">
+                    <div className="experience-entry-logo">
+                    <img src={process.env.PUBLIC_URL + 'bluestacks.jpg'} 
+                        className="experience-entry-logo-img" ></img>
+                    </div>
                 
-                <div className="experience-entry">
-                <div className="experience-entry-logo">
-                <img className="experience-entry-img"
-                    src={process.env.PUBLIC_URL+"/bluestacks.jpg"}></img>
-                </div>
-                <div className="experience-entry-connect"></div>
                 </div>
                 <div className="experience-entry">
-                <div className="experience-entry-logo">
-                <img className="experience-entry-img"
-                    src={process.env.PUBLIC_URL+"/gawds.jpg"}></img>
+                 <div className="experience-entry-logo">
+                 <img src={process.env.PUBLIC_URL + 'gawds.jpg'} 
+                        className="experience-entry-logo-img" ></img>
+                 </div>
                 </div>
-                <div className="experience-entry-connect"></div>
                 </div>
+               
             </div>
             </div>
         );
