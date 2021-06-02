9f9f90a420043886812cb545983eb28849bc4927
prathamzx <pratham.1999@yahoo.in>
2021-06-02 11:24:28 +0530

    Experience section added

--- portfolio_2/src/Components/Experience.js
+++ portfolio_2/src/Components/Experience.js
@@ -22,24 +22,67 @@ class Experience extends React.Component {
                 </div>
                 </div>
                 <div className="experience-entry">
+                    <div className="experience-logo-container">
                     <div className="experience-entry-logo">
                         <img src={process.env.PUBLIC_URL+ "/themes/" + this.props.theme + "/natwest.png"} 
                         className="experience-entry-logo-img" ></img>
                     </div>
+                    </div>
+                    <div className="experience-desc-container">
+                        <div className="experience-head">
+                            <span className="experience-head-title">Natwest Group</span> 
+                             
+                            <br/>
+                            <span className="experience-head-designation">Software Engineer</span> 
+                            <br/>
+                            <span className="experience-head-duration">July 2021 - Current</span> 
+                            <br/>
+                            <span className="experience-head-location">Gurugram, Haryana, India</span> 
+                        </div>
+                    </div>
                 </div>
                 <div className="experience-entry">
+                <div className="experience-logo-container">
                     <div className="experience-entry-logo">
-                    <img src={process.env.PUBLIC_URL+ "/themes/" + this.props.theme + "/bluestacks.png"} 
+                        <img src={process.env.PUBLIC_URL+ "/themes/" + this.props.theme + "/bluestacks.png"} 
                         className="experience-entry-logo-img" ></img>
+                    </div>
+                    </div>
+                    <div className="experience-desc-container">
+                        <div className="experience-head">
+                            <span className="experience-head-title">BlueStacks</span> 
+                             
+                            <br/>
+                            <span className="experience-head-designation">MTS - Intern</span> 
+                            <br/>
+                            <span className="experience-head-duration">Jan 2020 - June 2020</span> 
+                            <br/>
+                            <span className="experience-head-location">Gurugram, Haryana, India</span> 
+                        </div>
                     </div>
                 
                 </div>
                 <div className="experience-entry">
-                 <div className="experience-entry-logo">
-                 <img src={process.env.PUBLIC_URL+ "/themes/" + this.props.theme + "/gawds.png"}
+                <div className="experience-logo-container">
+                    <div className="experience-entry-logo">
+                        <img src={process.env.PUBLIC_URL+ "/themes/" + this.props.theme + "/techno.png"} 
                         className="experience-entry-logo-img" ></img>
-                 </div>
+                    </div>
+                    </div>
+                    <div className="experience-desc-container">
+                        <div className="experience-head">
+                            <span className="experience-head-title">Technobyte</span> 
+                             
+                            <br/>
+                            <span className="experience-head-designation">Website Committee Head</span> 
+                            <br/>
+                            <span className="experience-head-duration">June 2020 - May 2021</span> 
+                            <br/>
+                            <span className="experience-head-location">Kurukshetra, Haryana, India</span> 
+                        </div>
+                    </div>
                 </div>
+                
                 </div>
                
             </div>
